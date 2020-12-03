cb58e0d1923407c34890b78f75158ff254f4b607
Emanuele Spadaro <32676891+EmanueleS2@users.noreply.github.com>
2020-12-03 19:16:15 +0100

    Update LeagueRegion w/ API Clusters

--- RiotSharp/Enums/LeagueRegion.cs
+++ RiotSharp/Enums/LeagueRegion.cs
@@ -23,7 +23,12 @@ namespace LeagueCLUTest.RiotSharp.Enums
 
     static class LeagueRegionExtension
     {
-        public static string AsString(this LeagueRegion r)
+        /// <summary>
+        /// The subdomain name of a given region for region-specific RiotGames API Clusters
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public static string AsApiString(this LeagueRegion r)
         {
             switch (r)
             {
@@ -71,6 +76,35 @@ namespace LeagueCLUTest.RiotSharp.Enums
                     {
                         return string.Empty;
                     }
+            }
+        }
+
+        public static string AsString(this LeagueRegion r)
+        {
+            switch (r)
+            {
+                case LeagueRegion.BR1:
+                    return "BR";
+                case LeagueRegion.EUN1:
+                    return "EUNE";
+                case LeagueRegion.EUW1:
+                    return "EUW";
+                case LeagueRegion.JP1:
+                    return "JP";
+                case LeagueRegion.KR:
+                    return "KR";
+                case LeagueRegion.LA1:
+                    return "LAN";
+                case LeagueRegion.LA2:
+                    return "LAS";
+                case LeagueRegion.NA1:
+                    return "NA";
+                case LeagueRegion.OC1:
+                    return "OCE";
+                case LeagueRegion.TR1:
+                    return "TR";
+                default:
+                    return string.Empty;
             }
         }
     }
