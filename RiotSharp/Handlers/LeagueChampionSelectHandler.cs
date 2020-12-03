5c14e0b62cad1d3454bb45fb42a5641c4d7d65fb
Emanuele Spadaro <32676891+EmanueleS2@users.noreply.github.com>
2020-12-03 23:17:37 +0100

    Really Implement ChampionSelected event [TODOs]
    I've implemented our first event regarding ChampionSelect endpoint, now there's still plenty of things to do.
    We must complete ChampionSelectPhase enum, LeagueChampionSelectTimer & we must create a working model for the champion select "/session" resource

---
+++ RiotSharp/Handlers/LeagueChampionSelectHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueCLUTest.RiotSharp.Models;
+
+namespace LeagueCLUTest.RiotSharp.Handlers
+{
+    public class LeagueChampionSelectHandlerEventArgs : EventArgs
+    {
+        public int ChampionID { get; private set; }
+        public LeagueChampionSelectHandlerEventArgs(int c) => ChampionID = c; 
+    }
+
+    public class LeagueChampionSelectHandler
+    {
+        private LeagueSharp FatherLeagueSharp { get; set; }
+        /// <summary>
+        /// Polling Rate in milliseconds between requests
+        /// </summary>
+        private const int PollingRate = 2000;
+
+        private int previousChampionID = 0;
+
+        private Task RequestorTask;
+
+        public delegate void ChampionSelectedHandler(object sender, LeagueChampionSelectHandlerEventArgs e);
+        /// <summary>
+        /// Occurs when the player makes his final decision and picks a champion
+        /// </summary>
+        public event ChampionSelectedHandler ChampionSelected;
+
+        public LeagueChampionSelectHandler(LeagueSharp fatherLeagueSharp)
+        {
+            FatherLeagueSharp = fatherLeagueSharp;
+            RequestorTask = Task.Run(RequestorTaskCycle);
+        }
+
+        private async Task RequestorTaskCycle()
+        {
+            Console.WriteLine("CSHandler started");
+            while(true)
+            {
+                if(ChampionSelected != null)
+                {
+                    int champID = await FatherLeagueSharp.Requestor.ChampionSelect.GetCurrentChampionID();
+                    
+                    if (champID != 0 && champID != previousChampionID)
+                        ChampionSelected(this, new LeagueChampionSelectHandlerEventArgs(champID));
+
+                    previousChampionID = champID;
+                }
+
+                await Task.Delay(PollingRate);
+            }
+        }
+    }
+}
